ec8aaa5d66c5933454aacc27ce21c47d03d6fe2b
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-03-19 14:13:26 +0600

    Added views

---
+++ api/v1/views/course_view.py
@@ -0,0 +1,94 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status, viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from api.v1.permissions import IsStudentOrIsAdmin, ReadOnlyOrIsAdmin
+from api.v1.serializers.course_serializers import (CourseSerializer,
+                                                  CreateCourseSerializer,
+                                                  CreateGroupSerializer,
+                                                  CreateLessonSerializer,
+                                                  GroupSerializer,
+                                                  LessonSerializer)
+from api.v1.serializers.user_serializer import SubscriptionSerializer
+from courses.models import Course
+from users.models import Subscription
+
+
+class LessonViewSet(viewsets.ModelViewSet):
+    """Уроки."""
+
+    permission_classes = (IsStudentOrIsAdmin,)
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return LessonSerializer
+        return CreateLessonSerializer
+
+    def perform_create(self, serializer):
+        course = get_object_or_404(Course, id=self.kwargs.get('course_id'))
+        serializer.save(course=course)
+
+    def get_queryset(self):
+        course = get_object_or_404(Course, id=self.kwargs.get('course_id'))
+        return course.lessons.all()
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """Группы."""
+
+    permission_classes = (permissions.IsAdminUser,)
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return GroupSerializer
+        return CreateGroupSerializer
+
+    def perform_create(self, serializer):
+        course = get_object_or_404(Course, id=self.kwargs.get('course_id'))
+        serializer.save(course=course)
+
+    def get_queryset(self):
+        course = get_object_or_404(Course, id=self.kwargs.get('course_id'))
+        return course.groups.all()
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    """Курсы."""
+
+    queryset = Course.objects.all()
+    permission_classes = (ReadOnlyOrIsAdmin,)
+
+    def get_serializer_class(self):
+        if self.action in ['list', 'retrieve']:
+            return CourseSerializer
+        return CreateCourseSerializer
+
+    @action(
+        methods=['post'],
+        detail=True,
+        permission_classes=(permissions.IsAuthenticated,)
+    )
+    def subscribe(self, request, pk):
+        """Получение доступа к курсу (подписка на курс)."""
+
+        course = get_object_or_404(Course, id=pk)
+        subscription = Subscription.objects.create(
+            student=request.user,
+            course=course
+        )
+        serializer = SubscriptionSerializer(
+            subscription,
+            data=request.data,
+            context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        data = {
+            'message': 'Подписка на курс оформлена!',
+            'subscription': serializer.data
+        }
+        return Response(
+            data=data,
+            status=status.HTTP_201_CREATED
+        )
